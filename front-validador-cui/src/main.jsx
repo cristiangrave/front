950ef15727b6c5518e2ffdb65c96ae9e8a695793
Cristian Grave <crisg5284@gmail.com>
2024-12-20 11:42:27 -0600

    Mejorar la estructura del formulario de validación de CUI y simplificar el renderizado en main.jsx

--- front-validador-cui/src/main.jsx
+++ front-validador-cui/src/main.jsx
@@ -1,17 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Formulario from './Form/form'
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
   <>
-  <Container fluid>
-      <Row >
-          <Col>< Formulario/></Col>
-    </Row>
-    </Container>
+    < Formulario/>
   </>,
 )
